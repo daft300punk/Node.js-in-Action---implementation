e385c0fc6a93fdd9c09d5d76d5fa2e62e55e0461
Abhilash Singh <daft300punk@gmail.com>
2017-04-24 21:25:30 +0530

    Redis hash table

--- ch05/5.18.js
+++ ch05/5.18.js
@@ -10,3 +10,20 @@ client.get('color', (err, value) => {
   if (err) throw err;
   console.log('Got: ' + value);
 });
+
+client.hmset('camping', {
+  'shelter': '2-person tent',
+  'cooking': 'campstove'
+}, redis.print);
+
+client.hget('camping', 'cooking', (err, value) => {
+  if(err) throw err;
+  console.log('Will be cooking with: ' + value);
+});
+
+client.hkeys('camping', (err, keys) => {
+  if(err) throw err;
+  keys.forEach((key, i) => {
+    console.log(' ' + key);
+  });
+});
